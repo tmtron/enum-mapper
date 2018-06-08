18020cab8116686415db51f4e143c7580cf069b1
TMTron <martin.trummer@tmtron.com>
2018-06-08 09:32:21 +0200

    updated year in license headers

--- enum-mapper-lib/src/test/java/com/tmtron/enums/ColorEnum_MapperFull.java
+++ enum-mapper-lib/src/test/java/com/tmtron/enums/ColorEnum_MapperFull.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2017 Martin Trummer (martin.trummer@tmtron.com)
+ * Copyright © 2018 Martin Trummer (martin.trummer@tmtron.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
